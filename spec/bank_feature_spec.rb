b3a3fe472dab80da23f02514e60150f29555d4dd
Sophia Bell <sophbell87@gmail.com>
2020-04-21 22:05:37 +0100

    isolating print test, correct class test doubles

--- spec/bank_feature_spec.rb
+++ spec/bank_feature_spec.rb
@@ -4,27 +4,30 @@ require 'account'
 require 'statement'
 
 describe 'A user can interact with their bank account' do
+
+  let(:account) { Account.new }
+  let(:statement) {Statement.new(account.history)}
+
   before(:each) do
-    @account = Account.new
     @date = Time.now.strftime('%d/%m/%Y')
-    @account.deposit(100.00)
-    @account.withdraw(50.00)
-    @statement = Statement.new(@account.history)
   end
 
   it 'can deposit some money into the account' do
-    @account.deposit(50.00)
-    expect(@account.balance).to eq(100.00)
+    account.deposit(100.00)
+    expect(account.balance).to eq(100.00)
   end
 
   it 'can withdraw some money from the account' do
-    @account.withdraw(50.00)
-    expect(@account.balance).to eq(0)
+    account.deposit(100.00)
+    account.withdraw(50.00)
+    expect(account.balance).to eq(50.00)
   end
 
   it 'can print a statment' do
+    account.deposit(100.00)
+    account.withdraw(50.00)
     printed_statement = "date || credit || debit || balance\n#{@date} ||  || 50.00 || 50.00\n#{@date} || 100.00 ||  || 100.00\n"
 
-    expect { @statement.print }.to output(printed_statement).to_stdout
+    expect { statement.print }.to output(printed_statement).to_stdout
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,13 +4,15 @@ require 'timecop'
 require 'statement'
 
 describe Statement do
+
+  let(:account) { Account.new }
+
   it 'prints a blank statement when no transactions have been made' do
     blank = Statement.new([])
     expect { blank.print }.to output("date || credit || debit || balance\n").to_stdout
   end
 
   it 'prints a statement' do
-    account = Account.new
     Timecop.freeze(2020, 0o4, 19, 0, 0, 0) do
       account.deposit(100.00)
     end
