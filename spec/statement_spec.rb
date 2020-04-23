59f0befabab7429ab6a94f1f2dfc6c7e0388535b
Sophia Bell <sophbell87@gmail.com>
2020-04-23 14:39:43 +0100

    final mock added to statement spec

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require 'timecop'
 require 'statement'
 
 describe Statement do
-  let(:account) { Account.new }
+  let(:deposit) { double :transaction, credit: 100.00, debit: 0, date: '19/04/2020' }
+  let(:withdrawal) { double :transaction, credit: 0, debit: 50.00, date: '20/04/2020' }
 
   it 'prints a blank statement when no transactions have been made' do
     blank = Statement.new([])
@@ -12,13 +12,9 @@ describe Statement do
   end
 
   it 'prints a statement' do
-    Timecop.freeze(2020, 0o4, 19, 0, 0, 0) do
-      account.deposit(100.00)
-    end
-    Timecop.freeze(2020, 0o4, 20, 0, 0, 0) do
-      account.withdraw(50.00)
-    end
-    statement = Statement.new(account.history)
+    history = double('history')
+    history = [deposit, withdrawal]
+    statement = Statement.new(history)
     expect { statement.print_statement }.to output("date || credit || debit || balance\n20/04/2020 ||  || 50.00 || 50.00\n19/04/2020 || 100.00 ||  || 100.00\n").to_stdout
   end
 end
